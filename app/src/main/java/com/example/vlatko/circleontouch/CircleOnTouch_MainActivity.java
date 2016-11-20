f50a60807869271be4a34930f271210ca0c2c907
Vlaja-Gaja-Raja <wascke@gmail.com>
2016-11-20 17:55:13 +0100

    Add permission for camera on 6.0

--- app/src/main/java/com/example/vlatko/circleontouch/CircleOnTouch_MainActivity.java
+++ app/src/main/java/com/example/vlatko/circleontouch/CircleOnTouch_MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.vlatko.circleontouch;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -30,6 +34,8 @@ public class CircleOnTouch_MainActivity extends Activity implements CvCameraView
     private Mat mRgba;
     private boolean mTouch;
 
+    private static final int MY_PERMISSIONS_REQUEST_CAMERA = 417;
+
 
     private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
         @Override
@@ -56,10 +62,39 @@ public class CircleOnTouch_MainActivity extends Activity implements CvCameraView
         mTouch = false;
     }
 
+    private  void checkPremissionForCamera(){
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.CAMERA)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.CAMERA},
+                        MY_PERMISSIONS_REQUEST_CAMERA);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Log.i(TAG, "call onCreate");
         super.onCreate(savedInstanceState);
+        checkPremissionForCamera();
         //setContentView(R.layout.circle_on_touch_main_activity);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.circle_on_touch_main_activity);
